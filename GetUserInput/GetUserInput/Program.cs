eb182621b763083ceedad3bfc9bc958a23337ec6
Jared Prather <jprather7966@yahoo.com>
2019-07-01 08:06:13 -0500

    Created dev branch and added instructions

---
+++ GetUserInput/GetUserInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GetUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Using C# classes to implement, ask the user for information about themselves:
+                First Name
+                Last Name
+                Favorite Number
+                Number of Pets
+                Names of All Pets
+                Any other information you want to ask about
+            */
+
+
+        }
+    }
+}
